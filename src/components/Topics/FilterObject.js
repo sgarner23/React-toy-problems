458bc31ef5cb164342f5016899d98a1b77f76920
Stephen Garner <stephenallen.garner@gmail.com>
2021-10-21 14:25:33 -0600

    filter object complete

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { useState } from "react";
 
 const FilterObject = () => {
+  let originalArr = [
+    { name: "Stephen Garner", occupation: "The Boss" },
+    { name: "Mia", hobbies: "throwing fits" },
+    { name: "Chourtney", hairColor: "Brown" },
+  ];
+
+  const [userInput, setUserInput] = useState("");
+  const [filterText, setFilterText] = useState("Filtered: []");
+
+  function clickHandler() {
+    const filteredArr = originalArr.filter((item) => {
+      return userInput in item;
+    });
+
+    setFilterText(`Filtered: [${JSON.stringify(filteredArr)}]`);
+  }
+
   return (
-    <div>
-      <p>FilterObject</p>
+    <div className="puzzleBox evenAndOddPB">
+      <h4>Filter Object</h4>
+      <p className="original-arr"> Original: {JSON.stringify(originalArr)}</p>
+      <input
+        className="inputLine"
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      ></input>
+      <button className="confirmationButton" onClick={clickHandler}>
+        Filter
+      </button>
+      <span className="resultsBox filterObjectRB">{filterText}</span>
     </div>
   );
 };
