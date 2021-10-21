d7ad28863b6ecf3c3136e3a791eaa699039449a1
Stephen Garner <stephenallen.garner@gmail.com>
2021-10-21 15:31:01 -0600

    filter string

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,56 @@
 import React from "react";
+import { useState } from "react";
 
 const FilterString = () => {
+  let namesArr = [
+    "James",
+    "Jessica",
+    "Melody",
+    "Tyler",
+    "Blake",
+    "Jennifer",
+    "Mark",
+    "Maddy",
+  ];
+
+  const [userInput, setUserInput] = useState("");
+  const [filterText, setFilterText] = useState("Filtered Names: []");
+
+  function clickHandler() {
+    let newNamesArr = [];
+
+    namesArr.forEach((name) => {
+      if (name.includes(userInput)) {
+        console.log("It did include");
+        newNamesArr.push(" " + name);
+      } else {
+        console.log("This name wasn't pushed: ", name);
+      }
+    });
+
+    console.log("this is the new array: ", newNamesArr);
+
+    setFilterText(`Filtered Names: [${newNamesArr}]`);
+  }
+
   return (
-    <div>
-      <p>FilterString</p>
+    <div className="puzzleBox filterStringPB">
+      <h4>Filter String</h4>
+      <p>
+        Names: [ "James", "Jessica", "Melody", "Tyler", "Blake", "Jennifer",
+        "Mark", "Maddy" ]
+      </p>
+      <span className="puzzleText"></span>
+      <input
+        className="inputLine"
+        type="text"
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+      <button className="confirmationButton" onClick={clickHandler}>
+        Filter
+      </button>
+      <span className="resultsBox filterStringRB">{filterText}</span>
     </div>
   );
 };
